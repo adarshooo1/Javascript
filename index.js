967dc7ec045a114aa51166852238c6cc4dc7cf46
adarshooo1 <>
2023-03-23 16:53:18 +0000

    Day 1 Added In Javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+//Like this way we can print a statement in Jaascript.
+console.log("Hello World")
+
+//NOTE: There is no multiline concept in javascript.
+
+//Variable in Javascript:
+let a = 12;
+console.log(a)
+
+let word = "True"
+console.log(word)
+
+let num = 33.33
+console.log(num)
+
+
+
+//Let is easy to manipulate and change.
+let name = "Adarsh"
+console.log(name)
+name = "Deepanshu"
+console.log(name)
+
+var game = "pubg"
+game = "combat"
+console.log(game)

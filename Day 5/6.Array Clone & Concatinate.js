ddeb6694f2af4da8a22b9977ff6ea234c54b7763
Adarsh Singh <adarshsinghak001@gmail.com>
2023-04-01 15:58:48 +0530

    Day 5 Added In JavaScript

--- Day 5/6.Array Clone & Concatinate.js
+++ Day 5/6.Array Clone & Concatinate.js
@@ -5,6 +5,10 @@ let array2 = array1.slice(0)//With the help of slice method it will slice the ar
 console.log(array1)//#output:- [ 'item1', 'item2', 'itme3' ]
 console.log(array2)//#output:- [ 'item1', 'item2', 'itme3' ]
 
+//To add additional item in the array which which copy the item of one array and also add some other element inside the array as well.
+newArr1 = array1.splice(0).concat(["itemX" , "itemZ"])
+console.log(newArr1)
+
 array1.push("item4")
 
 //Here the original element is also changed but it doesn't effect the cloned array.
@@ -26,11 +30,26 @@ array3.push("Hax")
 console.log(array3)
 console.log(array4)
 
+//To add additional item in the array which which copy the item of one array and also add some other element inside the array as well.
+newArr2 = [].concat(array3,["itemA", "ItemB"])
+console.log(newArr2)
+
 //New way :- spread operator
 
 array5 = ["Max","Tax","Wax"]
-array6 = [...array5];
+array6 = [...array5];// Like this way we can also concatinate the array which will not reference to the other array.
 array5.push("Map")
 console.log(array5)
 console.log(array6)
 
+//To add additional item in the array which which copy the item of one array and also add some other element inside the array as well.
+newArr3 = [...array5,"ItemC","ItemD"]
+console.log(newArr3)
+
+
+/////////////////////////////////////////////////////////
+//To make a new array with the help of 2 different array short and simpler way.
+aArray = ["Batman", "Superman", "Spiderman"]
+bArray = [1 , 0 , undefined , "null" , null, false, "Aman", 2382.3," ",7]
+newArray = [...aArray, ...bArray]
+console.log(newArray)//here we get a new type of array which has the property of both of the array.

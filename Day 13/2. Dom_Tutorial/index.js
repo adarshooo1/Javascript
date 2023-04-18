75eee6e91e61edcd663b164f6c6c6711b74dbcc8
Adarsh Singh <adarshsinghak001@gmail.com>
2023-04-18 23:09:33 +0530

    Day 13 Added In Javascrit

--- Day 13/2. Dom_Tutorial/index.js
+++ Day 13/2. Dom_Tutorial/index.js
@@ -1,4 +1,6 @@
 // We are changing the object in the html DOM. what wver the content is there in the dom it will replaced with the dom.
- const greet = document.getElementById("hello")
- greet.textContent = "Good Morning"
+ const greet = document.getElementById("hello")//Here we select the id and now we can write the content with text.content. which we want to show in the webpage.
+ greet.textContent = "Enter your Name"
  console.log(greet.textContent)
+
+//what is document ,So document is a object in the windo which have various methods and function in it.

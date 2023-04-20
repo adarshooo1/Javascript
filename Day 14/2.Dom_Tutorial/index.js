10f71e6d262ba4f21490d9fbbaf6524441a3e1a1
Adarsh Singh <adarshsinghak001@gmail.com>
2023-04-20 22:35:54 +0530

    Day 14 Added In Javascrit

--- Day 14/2.Dom_Tutorial/index.js
+++ Day 14/2.Dom_Tutorial/index.js
@@ -5,11 +5,10 @@ console.log(navItems[0]);
 console.log(navItems[1]);
 console.log(navItems[2]);
 
-//Looping in getElementByClassName():
+//Looping in getElementByClassName(): Type of HTML Collection,
 for(i = 0 ; i < navItems.length; i++){
     navItems[i].style.background = "White";
 }
-
 
 //Looping in getElementByTagName():
 // We can use 2 types of loop in this:
@@ -32,12 +31,29 @@ for(let item of navItems2){
 
 //Get multiple elements using querySelectorAll:
 const navItems3 = document.querySelectorAll(".nav-item");//This will give the nodelist which is like array but not array.
-console.log(navItems2)
-console.log(navItems2[0]);
-console.log(navItems2[1]);
-console.log(navItems2[2]);
+console.log(navItems3)
+console.log(navItems3[0]);
+console.log(navItems3[1]);
+console.log(navItems3[2]);
 
-//Looping
+//Looping in querySelectorAll(): Type of NodeList:
+// we can use all type of loops in NodeList:
 
+// simple for loop 
+for(i= 0; i < navItems3.length; i++){
+    navItems3[i].style.backgroundColor = "red"
+    navItems3[i].style.fontWeight = "bold"
+    //Here we do not apply text color to the anchor tag item,So thats why we use method of getElementByTagName(); 
+}
 
+// for of loop
+for(let item of navItems3){
+    item.style.backGroundColour = "white"
+    item.style.fontSize = "2rem"
+}
 
+// for each loop
+navItems3.forEach(item =>{
+    item.style.backgroundColor = "purple"
+    item.style.fontSize = "1.3rem"
+})

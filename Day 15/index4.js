1d9a2e9268af341a3d61a0e471fe5e62d589486e
Adarsh Singh <adarshsinghak001@gmail.com>
2023-04-23 10:14:49 +0530

    Day 15 Added In Javascript

--- Day 15/index4.js
+++ Day 15/index4.js
@@ -4,15 +4,27 @@
 // querySelectorAll will give static list;Like Node List.
 // getElementby...... will give the live list.Like HTML collection.
 
-const ul2 = document.querySelector(".todo-list")
-const listItem = ul.getElementsByTagName("li")
+
+// HTML COllection for the live list:
+const ul2 = document.querySelector(".todo-list");//Here We selecting the class name of the ul tag in which we have to add the new-todo li;
+const seventhLi = document.createElement('li');//Then we will create the li item by createElement;
+seventhLi.textContent = "Seventh Todo";//Then will will write the li item what is should like or what content should be inside the list item.
+ul2.append(seventhLi);//Here we add the seventhLi with the ul tag with all the text content and changes;
+
+const listItem = ul2.getElementsByTagName("li");//Here we select the ul Tag with all the list item inside the todo-list.
+console.log(listItem);//print all the li item.
 
 
-const seventhLi = document.createElement('li');
-seventhLi.textContent = "Seventh Todo";
 
-ul2.append(seventhLi);
-console.log(listItem)
+
+// Node List for the static list:
+const ul3 = document.querySelectorAll(".todo-list li");
+const eighthLi = document.createElement("li");
+eighthLi.textContent = "Eight Todo";
+const ul = document.querySelector(".todo-list");
+ul.append(eighthLi);
+
+console.log(ul3);
 
 
 
